1d90143b4c105f0cb847a0f517640ab0fe81d8a1
zohaibxrehman <zohaibxrehman@gmail.com>
2020-06-14 05:05:07 -0400

    Implemented bar chart

--- src/App.js
+++ src/App.js
@@ -24,13 +24,13 @@ class App extends React.Component {
 	}
 
 	render() {
-		const { data } = this.state;
+		const { data, country } = this.state;
 
 		return (
 			<div className={styles.container}>
-				<Cards data={ data }/>
 				<CountryPicker handleCountryChange={this.handleCountryChange}/>
-				<Chart data={data}/>
+				<Cards data={ data }/>
+				<Chart data={data} country={country}/>
 			</div>
 		);
 	}
