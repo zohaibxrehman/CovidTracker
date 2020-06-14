9fce77541c848ab2486a4976f0fda187799508e1
zohaibxrehman <zohaibxrehman@gmail.com>
2020-06-14 00:27:58 -0400

    Covid data loaded into data state.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 
 import { Cards, Chart, CountryPicker } from './components';
+import { fetchData } from './api';
 
 class App extends React.Component {
+	constructor() {
+		this.state = {
+			data: {}
+		}
+	}
+
+	async componentDidMount() {
+		const fetchedData = await fetchData();
+		this.setState({ data: fetchedData });
+	}
+
 	render() {
 		return (
 			<div>
-				<h1>App</h1>
+				<Cards />
+				<CountryPicker />
+				<Chart />
 			</div>
 		);
 	}
