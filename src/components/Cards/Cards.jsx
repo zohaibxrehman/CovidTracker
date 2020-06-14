7fabc153ba0eb51675b886d92809de1412d750de
zohaibxrehman <zohaibxrehman@gmail.com>
2020-06-14 00:59:46 -0400

    Set up card structure.

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,8 +1,63 @@
 import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 
-const Cards = () => {
+import styles from './Cards.module.css';
+
+const Cards = (props) => {
+    console.log(props);
     return (
-        <h1>Cards</h1>
+        <div className={styles.container}>
+            <Grid container spacing={3} justify="center">
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>
+                            Infected
+                        </Typography>
+                        <Typography variant="h5">
+                            DATA
+                        </Typography>
+                        <Typography color="textSecondary">
+                            DATE
+                        </Typography>
+                        <Typography variant="body2">
+                            Number of active COVID-19 cases.
+                        </Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>
+                            Recovered
+                        </Typography>
+                        <Typography variant="h5">
+                            DATA
+                        </Typography>
+                        <Typography color="textSecondary">
+                            DATE
+                        </Typography>
+                        <Typography variant="body2">
+                            Number of recovered COVID-19 cases.
+                        </Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>
+                            Deaths
+                        </Typography>
+                        <Typography variant="h5">
+                            DATA
+                        </Typography>
+                        <Typography color="textSecondary">
+                            DATE
+                        </Typography>
+                        <Typography variant="body2">
+                            Number of COVID-19 deaths.
+                        </Typography>
+                    </CardContent>
+                </Grid>
+            </Grid>
+        </div>
     )
 }
 
