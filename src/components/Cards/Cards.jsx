5c2ee831b68a767d6da3fe5fa7816e0775f96d1b
zohaibxrehman <zohaibxrehman@gmail.com>
2020-06-14 01:14:11 -0400

    Api data added into cards.

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import CountUp from 'react-countup';
 
 import styles from './Cards.module.css';
 
-const Cards = (props) => {
-    console.log(props);
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+    if(!confirmed) {
+        return 'Loading...';
+    }
+
     return (
         <div className={styles.container}>
             <Grid container spacing={3} justify="center">
@@ -14,10 +18,10 @@ const Cards = (props) => {
                             Infected
                         </Typography>
                         <Typography variant="h5">
-                            DATA
+                            <CountUp start={0} end={confirmed.value} duration={1.5} separator=',' />
                         </Typography>
                         <Typography color="textSecondary">
-                            DATE
+                            {new Date(lastUpdate).toDateString()}
                         </Typography>
                         <Typography variant="body2">
                             Number of active COVID-19 cases.
@@ -30,10 +34,10 @@ const Cards = (props) => {
                             Recovered
                         </Typography>
                         <Typography variant="h5">
-                            DATA
+                            <CountUp start={0} end={recovered.value} duration={1.5} separator=',' />
                         </Typography>
                         <Typography color="textSecondary">
-                            DATE
+                            {new Date(lastUpdate).toDateString()}
                         </Typography>
                         <Typography variant="body2">
                             Number of recovered COVID-19 cases.
@@ -46,10 +50,10 @@ const Cards = (props) => {
                             Deaths
                         </Typography>
                         <Typography variant="h5">
-                            DATA
+                            <CountUp start={0} end={deaths.value} duration={1.5} separator=',' />
                         </Typography>
                         <Typography color="textSecondary">
-                            DATE
+                            {new Date(lastUpdate).toDateString()}
                         </Typography>
                         <Typography variant="body2">
                             Number of COVID-19 deaths.
@@ -58,7 +62,7 @@ const Cards = (props) => {
                 </Grid>
             </Grid>
         </div>
-    )
+    );
 }
 
 export default Cards;
