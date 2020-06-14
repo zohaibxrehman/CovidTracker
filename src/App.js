b8edc261698707f38a74e831a50369ba5a7e08e1
zohaibxrehman <zohaibxrehman@gmail.com>
2020-06-14 08:56:19 -0400

    Split grid to accomodate Tweet component.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import { Cards, Chart, CountryPicker } from './components';
+import { Cards, Chart, CountryPicker, Tweet } from './components';
+import { Grid } from '@material-ui/core';
 import { fetchData } from './api';
 import styles from './App.module.css';
 import image from './images/covidtracker.png';
@@ -28,12 +29,19 @@ class App extends React.Component {
 		const { data, country } = this.state;
 
 		return (
-			<div className={styles.container}>
-				<img className={styles.image} src={image} alt="COVID-19" />
-				<CountryPicker handleCountryChange={this.handleCountryChange}/>
-				<Cards data={ data }/>
-				<Chart data={data} country={country}/>
-			</div>
+			<Grid container spacing={1}>
+				<Grid item md={8} xs={12}>
+				<div className={styles.container}>
+					<img className={styles.image} src={image} alt="COVID-19" />
+					<CountryPicker handleCountryChange={this.handleCountryChange}/>
+					<Cards data={ data }/>
+					<Chart data={data} country={country}/>
+				</div>
+				</Grid>
+				<Grid item md={4} xs={12}>
+					<Tweet />
+				</Grid>
+			</Grid>
 		);
 	}
 }
