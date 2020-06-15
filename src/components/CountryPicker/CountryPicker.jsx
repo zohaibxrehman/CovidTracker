b70762a67851cb8b538d0a3ef5bc5d16fcda2b91
zohaibxrehman <zohaibxrehman@gmail.com>
2020-06-15 02:19:33 -0400

    Included JSON file with iso2 to coordinates mapping.

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,28 +1,32 @@
-import React, {useState, useEffect} from 'react';
-import {FormControl, Select} from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+import { FormControl, Select } from '@material-ui/core';
 
 import { fetchCountries } from '../../api';
 import styles from './CountryPicker.module.css';
 
-const CountryPicker = ({handleCountryChange}) => {
-    const [countries, setCountries] = useState([]);
+const CountryPicker = ({ handleCountryChange }) => {
+	const [ countries, setCountries ] = useState([]);
 
-    useEffect(() => {
-        const fetchAPI = async () => {
-        setCountries(await fetchCountries());
-        };
+	useEffect(() => {
+		const fetchAPI = async () => {
+			setCountries(await fetchCountries());
+		};
 
-        fetchAPI();
-    }, []);
+		fetchAPI();
+	}, []);
 
-    return (
-        <FormControl variant="outlined" className={styles.formControl}>
-            <Select native defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
-            <option value="">Global</option>
-            {countries.map((country, i) => <option key={i} value={country}>{country}</option>)}
-            </Select>
-        </FormControl>
-    );
+	return (
+		<FormControl variant="outlined" className={styles.formControl}>
+			<Select native defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
+				<option value="">Global</option>
+				{countries.map((country, i) => (
+					<option key={i} value={country.name}>
+						{country.name}
+					</option>
+				))}
+			</Select>
+		</FormControl>
+	);
 };
 
 export default CountryPicker;
