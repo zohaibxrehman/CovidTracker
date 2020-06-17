c8357d0b9912acc1665203defa93dd6077ffa7aa
zohaibxrehman <zohaibxrehman@gmail.com>
2020-06-17 05:47:51 -0400

    Updated map custom styling.

--- src/components/Map/mapStyles.js
+++ src/components/Map/mapStyles.js
@@ -1,104 +1,101 @@
 export default [
-	{
-		featureType: 'all',
-		elementType: 'labels',
-		stylers: [
-			{
-				visibility: 'off'
-			}
-		]
-	},
-	{
-		featureType: 'landscape',
-		elementType: 'all',
-		stylers: [
-			{
-				visibility: 'on'
-			},
-			{
-				color: '#f3f4f4'
-			}
-		]
-	},
-	{
-		featureType: 'landscape.man_made',
-		elementType: 'geometry',
-		stylers: [
-			{
-				weight: 0.9
-			},
-			{
-				visibility: 'off'
-			}
-		]
-	},
-	{
-		featureType: 'poi.park',
-		elementType: 'geometry.fill',
-		stylers: [
-			{
-				visibility: 'on'
-			},
-			{
-				color: '#83cead'
-			}
-		]
-	},
-	{
-		featureType: 'road',
-		elementType: 'all',
-		stylers: [
-			{
-				visibility: 'on'
-			},
-			{
-				color: '#ffffff'
-			}
-		]
-	},
-	{
-		featureType: 'road',
-		elementType: 'labels',
-		stylers: [
-			{
-				visibility: 'off'
-			}
-		]
-	},
-	{
-		featureType: 'road.arterial',
-		elementType: 'all',
-		stylers: [
-			{
-				visibility: 'on'
-			},
-			{
-				color: '#fee379'
-			}
-		]
-	},
-	{
-		featureType: 'road.highway',
-		elementType: 'all',
-		stylers: [
-			{
-				visibility: 'on'
-			},
-			{
-				color: '#fee379'
-			}
-		]
-	},
-	{
-		featureType: 'water',
-		elementType: 'all',
-		stylers: [
-			{
-				visibility: 'on'
-			},
-			{
-				color: '#7fc8ed'
-			}
-		]
-	}
-];
+    {
+        "featureType": "all",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": "32"
+            },
+            {
+                "lightness": "-3"
+            },
+            {
+                "visibility": "on"
+            },
+            {
+                "weight": "1.18"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.man_made",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": "-70"
+            },
+            {
+                "lightness": "14"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": "100"
+            },
+            {
+                "lightness": "-14"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            },
+            {
+                "lightness": "12"
+            }
+        ]
+    }
+]
