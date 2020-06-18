fa42faf4528d95763daee6f30cefd39c5e5aa411
zohaibxrehman <zohaibxrehman@gmail.com>
2020-06-18 01:01:49 -0400

    Replaced geocoding method with JSON data

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import countryToLocation from './countryToLocation';
 
 const url = 'https://covid19.mathdro.id/api';
 
@@ -50,30 +51,70 @@ export const fetchCountries = async () => {
 	}
 };
 
+// export const oldFetchCompleteData = async () => {
+// 	try {
+// 		const { data: { countries } } = await axios.get(`${url}/countries`);
+// 		let countriesData = [];
+// 		countries.forEach(async (country) => {
+// 			let response = await axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
+// 				params: {
+// 					address: 'country ' + country.name,
+// 					key: process.env.REACT_APP_GOOGLE_KEY
+// 				}
+// 			});
+// 			if (response.status === 200 && response.data.results.length > 0) {
+// 				let { lat, lng } = response.data.results[0].geometry.location;
+// 				let countryCases = await axios.get(`${url}/countries/${country.name}`);
+// 				if (country && !isNaN(lat) && countryCases && !isNaN(countryCases.data.confirmed.value)) {
+// 					let scale = 1 + 1.1 * Math.log2(countryCases.data.confirmed.value);
+// 					countriesData.push({
+// 						name: country.name,
+// 						location: { lat: lat, lng: lng },
+// 						...countryCases.data,
+// 						scale: scale
+// 					});
+// 				}
+// 			} else {
+// 			}
+// 		});
+// 		return countriesData;
+// 	} catch (error) {
+// 		console.log(error);
+// 	}
+// };
+
+// export const fetchMaster = async () => {
+// 	const { data: { countries } } = await axios.get(`${url}/countries`);
+// 	let countriesData = {};
+// 	countries.forEach(async (country) => {
+// 		let { lat, lng } = countryToLocation[country.name];
+// 		console.log(`${country.name}: `, countryToLocation[country.name]);
+// 		countriesData[country.name] = { lat: lat, lng: lng };
+// 		// }
+// 	});
+// 	console.log('a:', countryToLocation);
+// 	console.log(countriesData);
+// };
+
 export const fetchCompleteData = async () => {
-	try{
+	try {
 		const { data: { countries } } = await axios.get(`${url}/countries`);
 		let countriesData = [];
 		countries.forEach(async (country) => {
-			let response = await axios.get('https://maps.googleapis.com/maps/api/geocode/json', {params: {
-				address: 'country ' + country.name,
-				key: process.env.REACT_APP_GOOGLE_KEY
-			}});
-			if(response.status === 200 && response.data.results.length > 0){
-				// console.log('res:', response, response.status);
-				let { lat, lng } = response.data.results[0].geometry.location;
-				let countryCases = await axios.get(`${url}/countries/${country.name}`);
-				// console.log(`${country.name}: (${lat}, ${lng}), ${countryCases}`)
-				if(country && !isNaN(lat) && countryCases && !isNaN(countryCases.data.confirmed.value)){
-					let scale = 1 + 1.1 * Math.log2(countryCases.data.confirmed.value);
-					countriesData.push({name: country.name, location: {lat: lat, lng: lng}, ...countryCases.data, scale: scale});
-				}
-			} else {
-				// console.log('error:', response)
+			let { lat, lng } = countryToLocation[country.name];
+			let countryCases = await axios.get(`${url}/countries/${country.name}`);
+			if (country && !isNaN(lat) && countryCases && !isNaN(countryCases.data.confirmed.value)) {
+				let scale = 1 + 1.1 * Math.log2(countryCases.data.confirmed.value);
+				countriesData.push({
+					name: country.name,
+					location: { lat: lat, lng: lng },
+					...countryCases.data,
+					scale: scale
+				});
 			}
 		});
 		return countriesData;
-	} catch(error) {
+	} catch (error) {
 		console.log(error);
-	}	
-}
+	}
+};
